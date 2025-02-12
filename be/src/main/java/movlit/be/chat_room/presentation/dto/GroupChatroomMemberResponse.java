2e65026f18669cfc74c12c5cd4032c86a00c7e93
minjikimkim2222 <olivia0929@naver.com>
2025-02-12 10:12:23 +0900

    프로필 업데이트, 가입 리팩토링

--- be/src/main/java/movlit/be/chat_room/presentation/dto/GroupChatroomMemberResponse.java
+++ be/src/main/java/movlit/be/chat_room/presentation/dto/GroupChatroomMemberResponse.java
@@ -1,17 +1,27 @@
 package movlit.be.chat_room.presentation.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import movlit.be.common.util.ids.MemberId;
+import movlit.be.member.domain.entity.MemberEntity;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString
 public class GroupChatroomMemberResponse {
 
     private MemberId memberId;
     private String nickname;
     private String profileImgUrl;
+
+    public static GroupChatroomMemberResponse from(MemberEntity memberEntity){
+        return new GroupChatroomMemberResponse(
+                memberEntity.getMemberId(),
+                memberEntity.getNickname(),
+                memberEntity.getProfileImgUrl()
+        );
+    }
 
 }
