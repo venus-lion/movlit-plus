e8a4fef85127521f86becd003abd4724e07972ff
devbattery <devbattery@outlook.com>
2025-02-12 22:35:39 +0900

    Fix(be): rest docs 관련 수정

--- be/src/main/java/movlit/be/common/RestDocsController.java
+++ be/src/main/java/movlit/be/common/RestDocsController.java
@@ -1,6 +1,5 @@
 package movlit.be.common;
 
-import java.io.IOException;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
@@ -14,17 +13,14 @@ public class RestDocsController {
 
     @GetMapping("/api/docs")
     public ResponseEntity<Resource> getDocs() {
-        // 정적 리소스 로드
         Resource resource = new ClassPathResource("static/docs/index.html");
 
         if (!resource.exists()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
-        // Content-Type 설정 (HTML 파일) 및 리소스 반환.
         return ResponseEntity.ok()
-                .contentType(MediaType.TEXT_HTML)
-                .body(resource);
+                .contentType(MediaType.TEXT_HTML).body(resource);
     }
 
 }
